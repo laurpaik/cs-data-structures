b5f68eb85f5023f7fda32d77f5fa93230cff85c6
laurpaik <laurpaik@gmail.com>
2017-03-15 16:49:41 -0400

    Attempt lab

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,5 +1,57 @@
 # frozen_string_literal: true
 
-# An example class
+# A LinkedList class
 class LinkedList
+  # a node in the list
+  class Node
+    attr_accessor :val, :succ
+
+    # by default, the successor is nil
+    def initialize(val, succ = nil)
+      @val = val
+      @succ = succ
+    end
+  end
+
+  def initialize(val)
+    @head = Node.new(val, nil)
+  end
+
+  def append(val)
+    current = head
+    current = current.succ while current.succ
+    current.succ = Node.new(val, nil)
+  end
+
+  def prepend(val)
+    head.val = val
+    # without self, it can't tell if head is the local variable or the method
+    # moves head down one
+    self.head = Node.new(nil, head)
+    self
+  end
+
+  def to_s
+    node = head.succ
+    result = ''
+    while node
+      result += (result.empty? ? '' : ', ') + node.val.to_s
+      node = node.succ
+    end
+    result
+  end
+
+  # def remove(val)
+  #   current.succ = @head
+  #   if current.val = val
+  #     @head = current.succ
+  #   else
+  #     while (current.succ != nil) && (current.succ.val != val)
+  #       current = current.succ
+  #     end
+  #     unless current.succ == nil
+  #       current.succ = current.succ.next
+  #     end
+  #   end
+  # end
 end
